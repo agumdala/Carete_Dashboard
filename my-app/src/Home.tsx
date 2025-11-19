4eac89102b3b6439ef6a353a436af601fe00c73a
Arnav Gumdala <agumdala@gmail.com>
2025-11-18 18:10:02 -0800

    Homepage init

---
+++ my-app/src/Home.tsx
@@ -0,0 +1,13 @@
+// src/assets/Home.tsx
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello from HomeComponent!</h1>
+      <p>This is a reusable page-level component.</p>
+    </div>
+  );
+};
+
+export default Home;
